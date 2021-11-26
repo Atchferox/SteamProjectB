0f3af002c32d8afd7e777747e0686ba5bf3f9669
Luuk <Luukmunneke@gmail.com>
2021-11-26 13:01:09 +0100

    Added dasboard

---
+++ dashboard.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+dashboardwindow = Tk()
+
+dashboardwindow.title("Dashboard")  # Title van de window
+dashboardwindow.state('zoomed')  # Maakt de window fullscreen
+dashboardwindow.configure(background="#222831")
+
+
+
+mainloop()
