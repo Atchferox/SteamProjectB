c0dad7844951277e476d49a07aeca11f765a7599
Luuk <Luukmunneke@gmail.com>
2021-12-07 14:24:14 +0100

    comments toegevoegd

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -14,9 +14,11 @@ def sorting_data(databestand):
     i = 0
     dic = {}
     while i < len(data):
+        # Voegt de naam en de release date toe aan een dictionary
         dic[data[i]['name']] = data[i]['release_date']
         i += 1
 
+    # Sorteert de dictionary aan de hand van de values
     sortdict = sorted(dic.items(), key=lambda x: x[1])
     return sortdict
 
@@ -29,10 +31,11 @@ dashboardwindow.state('zoomed')  # Fullscreen
 # Standaard achtergrond kleur
 dashboardwindow.configure(background=bgcolor)
 
-label1 = Label(text=eerstespel)
+label1 = Label(text=eerstespel)  # Toont het eerste spel uit het bronbestand
 label1.configure(font=(font_tuple), bg=bgcolor, fg='white')
 label1.pack()
 
+# toont de gesorteerde dictionary van spellen op release datum
 label2 = Label(text=sorting_data(data), wraplength=1000)
 label2.configure(font=(font_tuple), bg=bgcolor, fg='white')
 label2.pack()
