a7d2410e0d21d238bf0a37ad3553ca9161a3b5b5
Thomas <thomas.pauw@student.hu.nl>
2021-12-14 18:47:11 +0100

    Staafdiagram toegevoegd bij library. Staafdiagram wordt nog wel op het verkeerde scherm getoond, moet nog even verplaatst worden naar het juiste scherm. Maar de functionaliteit is er.
    Vereist ook het installeren van matplotlib module

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -1,15 +1,15 @@
 from tkinter import *
 import json
-
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import requests
 from ctypes import windll
-
 
 # Deze setting zorgt ervoor dat op elk soort display het dashboard scherp is
 windll.shcore.SetProcessDpiAwareness(1)
 
 bgcolor = "#222831"  # Achtergrond kleur
 font_tuple = ("Montserrat Extra Light", 20)  # Font
-
 
 with open('deelsteam.json', 'r') as f:
     data = json.load(f)
@@ -26,16 +26,53 @@ def sorting_data(databestand):
 
     # Sorteert de dictionary aan de hand van de values
     sortdict = sorted(dic.items(), key=lambda x: x[1])
-
     return sortdict
-
 
 def getselectedelement(event):
     selection = event.widget.curselection()
     index = selection[0]
     value = event.widget.get(index)
+    produce_bar(value) #Diagrammen bij het geselecteerde spel worden opgehaald
     return value
 
+def produce_bar(value):
+    '''Maakt een tabel voor het weergeven van het percentage van positieve / negatieve reviews'''
+    try: #Voor in het geval dat er nog geen diagram wordt getoond
+        clear_figures()
+        pass
+    except NameError:
+        pass
+
+    f = open('deelsteam.json', 'r')
+    data = json.load(f)
+    i = 0
+    dic = {}
+    while i < len(data):
+        # Voegt de naam en de release date en appid! toe aan een dictionary
+        dic[data[i]['name']] = data[i]['release_date'], data[i]['appid']
+        i += 1
+
+    appid = dic[value][1]
+
+    request = requests.get(f'https://steamspy.com/api.php?request=appdetails&appid={appid}')
+    data = request.json()
+
+    names = ['Positive reviews', 'Negative reviews']
+    values = [(data['positive'] / (data['positive'] + data['negative'])) * 100,
+              (data['negative'] / (data['positive'] + data['negative'])) * 100]
+
+    review_bar = Figure(figsize=(5, 4), dpi=100)
+    review_bar.add_subplot(111).bar(names, values)
+    review_bar.suptitle('Reviews')
+
+    global canvas
+    canvas = FigureCanvasTkAgg(review_bar, dashboardwindow)
+    canvas.draw()
+    canvas.get_tk_widget().pack(pady=100)
+
+def clear_figures():
+    '''Maakt de canvas van de staafdiagram leeg'''
+    canvas.get_tk_widget().destroy()
 
 def gamewindow():
 
