6d5c95326ef5f12715ce0fd9cd1c5beeedd65064
Luuk <Luukmunneke@gmail.com>
2022-01-12 15:18:31 +0100

    nieuwe dingen toegevoegd

--- Dashboard/newgui.py
+++ Dashboard/newgui.py
@@ -2,23 +2,33 @@ import PySimpleGUI as sg
 import json
 
 sg.theme('darkgray10')
+font = ("Montserrat Extra Light", 20)
 
 with open('deelsteam.json', 'r') as f:
     data = json.load(f)
     eerstespel = data[0]["name"]
 
+menu_def = [['Steam', ['Friends::friendskey', 'Help', 'About', '---', 'Contact Steam', '---', 'Exit::exitkey']],
+            ['Library', ['Games']]
+            ]
 
 layout = [
-    [sg.Text(eerstespel)],
-    [sg.Button('Quit')]
+    [sg.Menu(menu_def)],
+    [sg.Text(eerstespel, justification='center', font=font)]
 ]
 
-window = sg.Window('Dashboard', layout)
+
+window = sg.Window('Steam', layout, resizable=True).maximize()
+window.maximize()
 
 
 while True:
     event, values = window.read()
 
+    if event == 'Exit::exitkey':
+        break
+
     if event == sg.WINDOW_CLOSED or event == 'Quit':
         break
+
 window.close()
