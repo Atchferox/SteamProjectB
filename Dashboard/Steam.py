8ebf7560230c3ecd2b44efd3a201ed9cbd2e7afa
Luuk <Luukmunneke@gmail.com>
2022-01-17 10:32:12 +0100

    overbodige file verwijderd en naam veranderd

--- Dashboard/Steam.py
+++ Dashboard/Steam.py
@@ -22,15 +22,45 @@ def dashboard():
 
 
 def Gamewindow():
-    layout2 = [[sg.Text('Dit is het gameswindow', font=font)]
-               ]
+    sortdictforgame = sorting_data(data)
+    len_max = 0
+    gamelijst = []
+    for name, x in sortdictforgame:
+        gamelijst.append(name)  # Insert de namen in de listbox
+
+        # Dit maakt de lengte van de listbox. Word nog aangepast naar een functie die een scrollbar toevoegt als de namen lang zijn.
+        if len(name) > len_max:
+            len_max = len(name)
+
+    layout2 = [
+        [sg.Text('Dit is het gameswindow', font=font)],
+        [sg.Listbox(
+            values=gamelijst, size=(len_max, len(gamelijst)),
+            font=font, select_mode=sg.LISTBOX_SELECT_MODE_SINGLE, key='listbox_g', enable_events=True)
+         ]
+    ]
     return sg.Window('Games', layout2, finalize=True, resizable=True)
 
 
 dashboard, window2 = dashboard(), None
 
+
+def sorting_data(data):
+    i = 0
+    dic = {}
+    while i < len(data):
+        # Voegt de naam en de release date toe aan een dictionary
+        dic[data[i]['name']] = data[i]['release_date'], data[i]['appid']
+        i += 1
+
+    # Sorteert de dictionary aan de hand van de values
+    sortdict = sorted(dic.items(), key=lambda x: x[1])
+    return sortdict
+
+
 while True:
     window, event, values = sg.read_all_windows()
+    print(window, event, values)
     if event == sg.WIN_CLOSED or event == 'Exit::exitkey':
         window.close()
 
@@ -44,4 +74,3 @@ while True:
         window2 = Gamewindow()
 
 window.close()
-
