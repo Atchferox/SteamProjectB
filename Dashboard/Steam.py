34bc92e761723b67d260af353c935cd26f28dda0
Luuk <Luukmunneke@gmail.com>
2022-01-26 14:36:59 +0100

    figure toegevoegd

--- Dashboard/Steam.py
+++ Dashboard/Steam.py
@@ -1,16 +1,18 @@
-from tokenize import Name
-from xml.dom.minidom import Attr
+from msvcrt import kbhit
 import PySimpleGUI as sg  # pip install PySimpleGUI
 from ctypes import windll
 from API.API import *
-import concurrent
+
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # Hierdoor is het op elk scherm high definition
 windll.shcore.SetProcessDpiAwareness(1)
 
 sg.theme('darkgray10')
-font = ("Montserrat Extra Light", 20)  # test font
-font2 = ("Montserrat Extra Light", 14)
+font = ("Montserrat Extra Light", 18)  # test font
+font2 = ("Montserrat Extra Light", 12)
 
 
 '''def game_lijst():
@@ -25,21 +27,60 @@ font2 = ("Montserrat Extra Light", 14)
     return gamelijst, len_max'''
 
 
+def draw_figure(canvas, figure):
+    ''''''
+    global figure_canvas_agg
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    return figure_canvas_agg
+
+
+def produce_bar_diagram(values):
+    '''Maakt staafdiagram'''
+
+    try:  # Indien er al een diagram bestaat
+        figure_canvas_agg.get_tk_widget().destroy()
+    except NameError:
+        pass
+
+    names = ['Positieve reviews', 'Negatieve reviews']
+    colors = ['#60B6E7', '#E06363']
+
+    fig = plt.figure(figsize=(3, 3), facecolor='#1C1E23')
+    fig.add_subplot(111).bar(names, values, width=0.4, align='center', color=colors)
+    plt.ylim(0, 100)
+    plt.tick_params(colors='white')
+
+    return draw_figure(window['-CANVAS-'].TKCanvas, fig)
+
+
 def dashboard():
-    topgames, max_len = top100games()
+    topgames, listofids = top100games()
 
     menu_def = [['Steam', ['Friends::friendskey', 'Help::help', 'About', '---', 'Contact Steam', '---', 'Exit::exitkey']],
                 ['Library', ['Games::Gameskey']]]  # Hier komen de menu opties in. ['menu'['alles wat in het menu komt']]
+
+    top5gameslayout = [[sg.Text('Top 10 Games van de afgelopen 2 weken', font=font)],
+                       [sg.Listbox(
+                           values=topgames, size=(30, len(topgames)), font=font2,
+                           select_mode=sg.LISTBOX_SELECT_MODE_SINGLE, key='listbox_t', bind_return_key=True,
+                           enable_events=True)]
+                       ]
+    search_game = [[sg.Text('Search Games', font=font)],
+                   [sg.Input(size=(25, 20), pad=(12, 12))]
+                   ]
+
+    figure_canvas = [[sg.Canvas(key='-CANVAS-')]]
     layout = [
         [sg.Menu(menu_def)],
-        [sg.Text('Top 100 Games van de afgelopen 2 weken', font=font)],
-        [sg.Listbox(
-            values=topgames, size=(max_len, len(topgames)), font=font2,
-            select_mode=sg.LISTBOX_SELECT_MODE_SINGLE, key='listbox_t', bind_return_key=True,
-            enable_events=True)]
-    ]
+        [sg.vtop(sg.Frame(title='', layout=search_game)),
+         sg.Frame(title='', layout=figure_canvas, border_width=0),
+         sg.vtop(sg.Frame(title='', layout=top5gameslayout, vertical_alignment='RIGHT'))]]
 
-    return sg.Window('Dashboard', layout, finalize=True, resizable=True, icon='img/steamlogo.ico')
+    return sg.Window(
+        'Dashboard', layout, size=(1280, 720),
+        finalize=True, resizable=True, icon='img/steamlogo.ico')
 
 
 def Game_window():
@@ -67,7 +108,19 @@ def friend_list_window():
 
 def friend_window():
 
-    layout4 = [[sg.Listbox(values=['geen games'], key='-GAMES-', size=(30, 10))],
+    steamname = values['-OUTPUT-']
+    keydicname = steamname[0]
+
+    steamid1 = search_name(keydicname, name_steamid)  # Geeft steamid om de lijst van games te krijgen
+    gameidlijst, gamenames = get_games(steamid1)
+
+    if gamenames == None:  # Als iemand geen games heeft
+        gamenames = ['Geen games']
+
+    else:
+        gamenames_id = dict(zip(gamenames, gameidlijst))
+
+    layout4 = [[sg.Listbox(values=gamenames, key='-GAMES-', size=(30, 10))],
                [sg.Text(key='-gekke-')]
                ]
 
@@ -117,9 +170,14 @@ while True:
     elif event == 'Games::Gameskey' and not window2:  # Opent Game window
         window2 = Game_window()
 
-    elif event == 'listbox_g':  # Window 2
-        name = values[event]
-        keydic = name[0]
+    elif event == 'listbox_t':  # Dashboard listbox
+
+        name = values[event][0]
+        appid = get_appid(name)
+        review_values = get_steamspy(appid, 'reviews')
+        review_percentage = [(review_values[0] / (review_values[0] + review_values[1])) * 100,
+                             (review_values[1] / (review_values[0] + review_values[1])) * 100]
+        produce_bar_diagram(review_percentage)
 
     elif event == 'Friends::friendskey' and not window3:  # Opent Friend List window
         window3 = friend_list_window()
@@ -135,13 +193,6 @@ while True:
 
     elif event == '-OUTPUT-':  # Als er op een naam word geklikt
         window4 = friend_window()
-        steamname = values['-OUTPUT-']
-        keydicname = steamname[0]
-
-        steamid1 = search_name(keydicname, name_steamid)  # Geeft steamid om de lijst van games te krijgen
-        gameidlijst, gamenames = get_games(steamid1)
-
-        
 
 
 window.close()
