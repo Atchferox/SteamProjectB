a73cf1465d922636f1d899044f3cb66ad41ac5a2
Maarten-HU <m.plagmeyer@gmail.com>
2022-02-01 14:17:41 +0100

    Cleanup en API weg

--- Dashboard/TI/online.py
+++ Dashboard/TI/online.py
@@ -16,32 +16,10 @@ GPIO.setup(pinRood, GPIO.OUT)
 GPIO.setup(pinGroen, GPIO.OUT)
 GPIO.setup(pinBlauw, GPIO.OUT)
 
-def online():
-    try:
-        while True:
-            #haal onlinestatus uit steam API
-            response = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=F7CD5F6E51D9114EC9D9C44EEBCA6FF7&steamids=76561198208819849")
-            data = response.json()
-            userstatus = data["response"]["players"][0]["personastate"]
-            print(userstatus)
 
-            Offline = userstatus == 0
-            Online = userstatus == 1
-            Busy = userstatus == 2
-            Away = userstatus == 3
-            Snooze = userstatus == 4
-            looking_to_trade = userstatus == 5
-            looking_to_play = userstatus == 6
-            #als user online led wordt groen
-            while Online:
-                GPIO.output(pinGroen, GPIO.HIGH)
-                break
-            else:
-                GPIO.output(pinGroen, GPIO.LOW)
+GPIO.output(pinRood, GPIO.LOW)
+GPIO.output(pinGroen, GPIO.LOW)
+GPIO.output(pinBlauw, GPIO.LOW)
+GPIO.output(pinGroen, GPIO.HIGH)
 
 
-            time.sleep(5)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-
-online()
